e95ddc05c80e28001d3d7dfc621ca03418f3e8b4
amira-ahmed2 <git>
2023-05-26 09:07:30 +0300

    add image

--- script/main.js
+++ script/main.js
@@ -3,13 +3,13 @@ let buttonpre = document.getElementById("buttonToggoler2");
 
 let meta = document.querySelector("link[id='links']");
 let theme =meta.getAttribute("href");
-console.log(theme)
 button.addEventListener("click",function(){
     if(theme==="./css/styleLight.css"){
         theme="./css/style.css"
-        console.log(theme)
     }else{
         theme="./css/styleLight.css"
     }
     meta.setAttribute("href",theme)
 });
+
+document.addEventListener('contextmenu', event => event.preventDefault());
